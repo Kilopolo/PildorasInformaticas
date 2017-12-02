a706e051da97aa6dbdfe38384cce8f12c87e79b5
Pablo Diaz Rubio <pablo23dr@gmail.com>
2017-12-02 05:09:51 +0100

    Initial commit

---
+++ src/Threads/BancoSinSincronizar.java
@@ -0,0 +1,24 @@
+package Threads;
+
+public class BancoSinSincronizar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+}
+
+class Banco {
+	
+	private final double [] cuentas;
+	public Banco() {
+		// TODO Auto-generated constructor stub
+	
+		cuentas = new double[100];
+	}
+	
+	
+}
